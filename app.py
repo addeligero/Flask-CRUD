3dabcaf2821b20fb22d19c7d624c6ef22593144f
ADRIAN <adriandeligero.1030@gmail.com>
2025-07-16 13:54:30 +0800

    unsecure routes

--- app.py
+++ app.py
@@ -6,6 +6,14 @@ app = Flask(__name__)
 def index():
     return "<h1>Hello, World!</h1>"
 
+@app.route('/add/<int:num1>/<int:num2>')
+def add(num1, num2):
+    
+    return f'{num1} + {num2} = {num1 + num2}'
+
+@app.route('/hi')
+def hello():
+    return "Hello from the hello function!"
 
 if __name__ == '__main__':
     app.run(debug=True, host='localhost', port=5000)

7f12ff15edcc0325b14c8a449df46acbb97a8de9
ADRIAN <adriandeligero.1030@gmail.com>
2025-07-17 14:23:08 +0800

    added file tp render welcome section

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask,request
+from flask import Flask,request, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def index():
@@ -9,18 +9,23 @@ def index():
 @app.route('/add/<int:num1>/<int:num2>')
 def add(num1, num2):
     
+    
     return f'{num1} + {num2} = {num1 + num2}'
 
-@app.route('/hi')
+@app.route('/welcome')
 def hello():
-    return "Hello from the hello function!"
+    return render_template('index.html')
 
 
 @app.route('/handle_params')
 def handle_params():
-    name = request.args.get('name')
-    age = request.args.get('age')
-    return f"Hello {name}, you are: {age}"
+    if 'name' in request.args.keys() and 'age' in request.args:
+        name = request.args.get('name')
+        age = request.args.get('age')
+        return f"Hello {name}, you are: {age}"
+    
+    else:
+        return "Please provide both 'name' and 'age' parameters in the query string."
 
 if __name__ == '__main__':
     app.run(debug=True, host='localhost', port=5000)

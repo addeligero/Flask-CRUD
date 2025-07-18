e0354c43b89367cb164e2206e60ef2f41f3454e8
ADRIAN <adriandeligero.1030@gmail.com>
2025-07-18 12:46:22 +0800

    file: added for form

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,request, render_template
+from flask import Flask,request, render_template, request
 
 app = Flask(__name__, template_folder='templates')
 
@@ -9,16 +9,18 @@ def index():
 @app.route('/add/<int:num1>/<int:num2>')
 def add(num1, num2):
     
-    
     return f'{num1} + {num2} = {num1 + num2}'
 
-@app.route('/welcome')
+# This route handles the form submission
+@app.route('/welcome', methods=['GET' ,'POST'])
 def hello():
-    name='Adrian'
-    list = {1,2,3,4,5}
+    if request.method == 'GET':
+        name='Adrian'
+        list = {1,2,3,4,5}
     return render_template('index.html', name=name, list=list)
 
 
+# This route handle parameters from the query string
 @app.route('/handle_params')
 def handle_params():
     if 'name' in request.args.keys() and 'age' in request.args:

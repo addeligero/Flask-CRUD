f29b7a5b2eb9dec09e1c5ccfd6340093679fdeaf
ADRIAN <adriandeligero.1030@gmail.com>
2025-07-23 14:55:12 +0800

    db: successfully implemented CR in CRUD

--- dbapplication/models.py
+++ dbapplication/models.py
@@ -4,7 +4,7 @@ from app import db
 class Person(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(50), nullable=False)
-    last_name = db.Column(db.String(50), nullable=False)  # ← fixed!
+    last_name = db.Column(db.String(50), nullable=False)  
     age = db.Column(db.Integer, nullable=False)
 
     def __repr__(self):
--- dbapplication/routes.py
+++ dbapplication/routes.py
@@ -1,10 +1,9 @@
-from flask import render_template,request
-
+from flask import render_template, request
 from models import Person
 
-def register_routes(app,db):
+def register_routes(app, db):
     @app.route('/')
     def index():
         people = Person.query.all()
-        return str(people)
-    
+        return '<br>'.join(f"{p.first_name} {p.last_name} - {p.age}" for p in people)
+  
